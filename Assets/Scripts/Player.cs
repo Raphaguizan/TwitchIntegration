1df12eabe6d9e283ac106acaef59ac5d0464fff1
DESKTOP-Q313TGL\rapha <raphaelguizan@gmail.com>
2021-10-26 12:46:49 -0300

    inicio da implementação do jogo do JII

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Player
+public class Player : MonoBehaviour
 {
     public string playerName;
 
